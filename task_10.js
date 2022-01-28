1f5956abbc79412955c8a2460f1d3d4edee2f88a
Sergej Khmara <sergej.khmara.01@privatbank.ua>
2022-01-28 12:08:16 +0200

    1

---
+++ task_10.js
@@ -0,0 +1,30 @@
+// Создать имплементацию функции `reverse`, которая принимает массив в качестве параметра,
+//     а возвращает массив только в обратном порядке.Функция должна содержать проверки:
+//     - Первый параметр обязателен и может принимать только массив
+//     - Генерировать ошибку если был передан пустой массив
+
+
+//const arr = [3,2,1];
+//reverse(arr); // [1,2,3]
+function  mrevers (arr) {
+
+    var result = [];
+    if (!Array.isArray(arr)) throw new Error('Первый параметр обязателен и может принимать только массив');
+    let i, length = arr.length;
+    if (arr.length == 0) throw new Error('Длина массива ноль');
+    for (i = 0; i < length; i = i + 1) {
+        // result.push(arr[i])
+        result[i] = arr [(arr.length - 1) - i]
+    }
+    return result;
+}
+
+    function callback() {
+        return ;
+    }
+
+
+const arr = [];
+
+let rez = mrevers (arr) //вызов функции
+console.log (rez);
---
+++ task_7.js
@@ -0,0 +1,17 @@
+// Код ниже получает из массива строк новый массив, содержащий их длины:
+var arr = ['abcd', 'abcde', 'ab', 'abc'];
+
+// var arrLength = [];
+// for (var i = 0; i < arr.length; i++) {
+// arrLength[i] = arr[i].length;
+// }
+// console.log( arrLength ); // 4,5,2,3
+
+
+// Перепишите выделенный участок:
+// используйте вместо цикла `for` метод `map`
+
+var lengths = arr.map(function(word) {
+    return  word.length;
+});
+console.log(lengths);
